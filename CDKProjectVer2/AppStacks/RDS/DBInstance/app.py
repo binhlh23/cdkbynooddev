7665fe1255cf8f0d435ee77e50740b5334354561
binhlh23 <84550003+binhlh23@users.noreply.github.com>
2021-06-05 22:34:42 +0700

    Delete CDKProjectVer2/AppStacks/RDS directory

--- CDKProjectVer2/AppStacks/RDS/DBInstance/app.py
+++
@@ -1,9 +0,0 @@
-
-from aws_cdk import core
-from stacks import rds_conf
-from stacks.rds_stack import EC2RDSStack
-
-app = core.App()
-rds_construct_id = 's'
-vpc = EC2RDSStack(app, rds_construct_id)
-app.synth()
